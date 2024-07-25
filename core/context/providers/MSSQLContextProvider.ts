fc1a5dc79df495298fd70f3b4f54779d6f2293d6
Erik Nodland <erik.nodland@shoothill.com>
2024-07-25 16:01:34 +0000

    added mssql

--- core/context/providers/MSSQLContextProvider.ts
+++ core/context/providers/MSSQLContextProvider.ts
@@ -13,22 +13,59 @@ class MSSQLContextProvider extends BaseContextProvider {
     type: "normal",
   };
 
+  async getConnection() {
+    const sql = require("mssql");
+    return sql.connect(this.options.connectionString); 
+  }
+
+   private async getTableNames(): Promise<string[]> {
+    const schema = this.options.schema ?? "dbo";
+    const sql = await this.getConnection();
+    let tablesInfoQuery = `
+      SELECT
+        *
+      FROM
+        INFORMATION_SCHEMA.TABLES`;
+
+    if (schema !== null) {
+      tablesInfoQuery += ` WHERE table_schema = '${schema}'`;
+    }
+    
+    const { recordset: tablesInfo } = await sql.query(tablesInfoQuery);
+    const retval = tablesInfo.map(
+      (tableInfo: any) => `${tableInfo.TABLE_SCHEMA}.${tableInfo.TABLE_NAME}`,
+    );
+    return retval;
+  }
+
+  private async getFieldNames(tableName: string): Promise<string[]> {
+    const sql = await this.getConnection();
+    const schema = this.options.schema ?? "public";
+    let fieldInfoQuery = `
+      Select *
+      FROM INFORMATION_SCHEMA.COLUMNS`;
+    if (tableName !== null) {
+      fieldInfoQuery += ` WHERE Table_Name = '${tableName}'`;
+    }
+    const { rows: fieldsInfo } = await sql.query(fieldInfoQuery);
+    return fieldsInfo.map(
+      (fieldInfo: any) => `${fieldInfo.table_schema}.${fieldInfo.table_name}`,
+    );
+  }
+
   async getContextItems(
     query: string,
     extras: ContextProviderExtras,
   ): Promise<ContextItem[]> {
-    console.log(this.options);
-    if ((await extras.ide.getIdeInfo()).ideType === "jetbrains") {
-      throw new Error(
-        "The terminal context provider is not currently supported in JetBrains IDEs",
-      );
-    }
-    const content = await extras.ide.getTerminalContents();
+    console.log("options", this.options);
+    const result = await this.getTableNames();
+
+    const content = result;
     return [
       {
-        description: "The contents of the terminal",
-        content: `Current terminal contents:\n\n${content}`,
-        name: "Terminal",
+        description: "The contents of the db",
+        content: `${content}`,
+        name: "MSSQL",
       },
     ];
   }
